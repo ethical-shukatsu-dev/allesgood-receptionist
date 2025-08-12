262862d64fbc93f74b79ed63c67f512724ab0cc3
kimly.888 <kimly.jet@gmail.com>
2025-08-12 10:44:40 +0900

    fix: correct spelling of team member's ID

--- lib/team.ts
+++ lib/team.ts
@@ -11,7 +11,7 @@ export const team: Team[] = [
   { id: 'kageyama_yumiko', nameEn: 'Kageyama Yumiko', nameJa: '影山 由美子', mention: 'U06UVF0JFLP' },
   { id: 'katanaka_hiroki', nameEn: 'Katanaka Hiroki', nameJa: '潟中 弘貴', mention: 'U06MJASEP4H' },
   { id: 'iijima_takuto', nameEn: 'Iijima Takuto', nameJa: '飯島 拓人', mention: 'D093C7A537E' },
-  { id: 'ota_mayaka', nameEn: 'Ota Mayaka', nameJa: '大田 麻弥加', mention: 'U07S2AJA094' },
+  { id: 'ohta_mayaka', nameEn: 'Ohta Mayaka', nameJa: '大田 麻弥加', mention: 'U07S2AJA094' },
   { id: 'nakamura_nagisa', nameEn: 'Nakamura Nagisa', nameJa: '中村 なぎさ', mention: 'D08TVC26MQX' },
   { id: 'hirayama_akane', nameEn: 'Hirayama Akane', nameJa: '平山 果音', mention: 'D099XD64YNR' },
   { id: 'kim_jaebeom', nameEn: 'Jaebeom Kim', nameJa: '金 宰範', mention: 'D08LFTP7R9P' },
